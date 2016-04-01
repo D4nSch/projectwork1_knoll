b7ddedf2b0519987acf8bfb130ab52e66ac05c84
Pepsigrummy <danielschaberreiter@Daniels-MacBook-Pro.local>
2016-04-01 19:47:48 +0200

    level beginning

--- Ball3D-Roll4Glory/Assets/Scripts/CameraController.cs
+++ Ball3D-Roll4Glory/Assets/Scripts/CameraController.cs
@@ -9,14 +9,19 @@ public class CameraController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 		offset = transform.position - player.transform.position;
+	}
+
+	void Update () {
+		transform.Translate(Vector3.forward * Time.deltaTime*1);
 
 	}
 	
 	// LateUpdate is called once per frame - after Update
 	void LateUpdate () {
 	
+
 		transform.position = player.transform.position + offset;
 
 	}
